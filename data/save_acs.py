df92cecd87e2e6ef9bf24140b4f39ff0e2080fc5
Max Ghenis <mghenis@gmail.com>
2021-07-10 13:41:55 -0700

    Add data and conduct initial analysis

---
+++ data/save_acs.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+p = pd.read_stata(
+    "https://www2.census.gov/programs-surveys/supplemental-poverty-measure/datasets/spm/spm_2018_pu.dta",
+    columns=[
+        "serialno",
+        "sporder",
+        "wt",
+        "age",
+        "spm_id",
+        "spm_povthreshold",
+        "spm_resources",
+    ],
+)
+
+p.to_csv("data/acs_poverty.csv.gz", compression="gzip", index=False)
